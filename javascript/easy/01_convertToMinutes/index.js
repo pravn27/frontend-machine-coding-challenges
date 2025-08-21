fc68e7a567a27e0171b0fb1058f3440d57cd507d
pravn27 <praveen.sri27@gmail.com>
2025-08-21 20:32:43 +0530

    added comment for function

--- javascript/easy/01_convertToMinutes/index.js
+++ javascript/easy/01_convertToMinutes/index.js
@@ -1,4 +1,8 @@
-// function to converts time from hours to minutes
+/**
+ * Converts time from hours to minutes.
+ * @param {number} hours - The number of hours to convert.
+ * @returns {number|string} The equivalent minutes, or an error message for invalid input.
+ */
 const convertToMinutes = (hours) => {
   if (hours > 0) {
     return hours * 60;
