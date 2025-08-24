b918978039db780a36a941025129bca7d595b56f
pravn27 <praveen.sri27@gmail.com>
2025-08-24 10:58:57 +0530

    adding array remove duplicates elements challenge

---
+++ javascript/easy/02_arrays/01/index.js
@@ -0,0 +1,27 @@
+// Write a function to remove duplicate element from an Array
+
+// Approach 1 - using Set()
+const removeDuplicates_A1 = (arr) => {
+  const uniqueElements = new Set(arr);
+  return [...uniqueElements];
+};
+
+console.log("Approach 1 =======>");
+console.log(removeDuplicates_A1([1, 2, 2, 3, 3, 3, 4, 5, 3, 2, 1]));
+console.log(removeDuplicates_A1([6, 5, 6, 1, 1, 2, 5, 2, 1]));
+console.log(removeDuplicates_A1(["Hare", "Hare", "Krishna", "Hare"]));
+console.log(removeDuplicates_A1(["1", 1, 2, 2, 3, 1, "1"]));
+
+// Approach 2 - using Array filter and indexOf methods
+const removeDuplicates_A2 = (arr) => {
+  const uniqueElements = arr.filter(
+    (item, index) => arr.indexOf(item) === index
+  );
+  return uniqueElements;
+};
+
+console.log("Approach 2 =======>");
+console.log(removeDuplicates_A2([1, 3, 3, 3, 4, 5, 3, 1]));
+console.log(removeDuplicates_A2([6, 5, 6, 1, 5, 2, 1]));
+console.log(removeDuplicates_A2(["Hare", "Rama", "Krishna", "Hare", "Hare"]));
+console.log(removeDuplicates_A2(["1", 1, 2, 3, 1, "1"]));
---
+++ javascript/easy/02_arrays/01/requirement&approach.md
@@ -0,0 +1,31 @@
+# Requirement or Problem statement & Solutioning Approach
+
+## 1. Problem statement
+
+- Write a function to remove duplicate element from an Array
+
+## 2. Features / Usecase
+
+## 3. Understand the problem with sample inputs & outputs
+
+### Sample - 1
+
+- Input: [1, 2, 2, 3, 3, 3, 4, 5, 3, 2, 1]
+- Output: [1, 2, 3, 4, 5]
+  - Should return `Unique Elements` only
+
+### Sample - 2
+
+- Input: [6, 5, 6, 1, 1, 2, 5, 2, 1]
+- Output: [1, 2, 5, 6]
+  - Should return `Unique Elements` only
+
+## 4. Approach
+
+### Approach - 1
+
+- using Set()
+
+### Approach - 2
+
+- Array filter method
